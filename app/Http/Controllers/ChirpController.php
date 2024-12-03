4219bff4905d07dfae076ac4318682104783befb
Bowonphong <teebowonphong@gmail.com>
2024-12-03 10:46:14 +0700

    chirper-c1

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -3,16 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Models\Chirp;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+// use Illuminate\Http\Response;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class ChirpController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    // public function index()
+    public function index(): Response
     {
-        //
+
+        // return response('Hello, World!');
+        return Inertia::render('Chirps/Index', [
+            //
+        ]);
+
     }
 
     /**
@@ -26,9 +36,16 @@ class ChirpController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    // public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $request->user()->chirps()->create($validated);
+
+        return redirect(route('chirps.index'));
     }
 
     /**
